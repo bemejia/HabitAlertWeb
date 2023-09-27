34f9476e53e3a749206ce3f1cdc6acc246b8c9b6
bemejia <b.mejial@uniandes.edu.co>
2023-09-26 23:41:16 -0500

    frame inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
 
 
 @NgModule({
-  declarations: [	
-    AppComponent
-     
+  declarations: [			
+    AppComponent,
+      HomeComponent
    ],
   imports: [
     BrowserModule,
